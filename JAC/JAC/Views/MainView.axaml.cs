78152e83feb52e0f236da74450eaa39b047aaca8
RaminKasumov <145226364+RaminKasumov@users.noreply.github.com>
2024-02-27 21:18:56 +0100

    Created projects for Chat

--- JAC/JAC/Views/MainView.axaml.cs
+++ JAC/JAC/Views/MainView.axaml.cs
@@ -1,11 +1,103 @@
+using System.Drawing;
+using System.Net;
+using System.Net.Sockets;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Media;
+using JAC.Shared;
+using Color = Avalonia.Media.Color;
 
 namespace JAC.Views;
 
 public partial class MainView : UserControl
 {
+    #region instancevariables
+    readonly int _bufferSize = 1024;
+
+    readonly Socket _clientSocket;
+        
+    readonly SocketReader _reader;
+        
+    readonly SocketWriter _writer;
+    #endregion
+    
+    #region constructor
     public MainView()
     {
         InitializeComponent();
+        _clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        _reader = new SocketReader(_clientSocket, _bufferSize);
+        _writer = new SocketWriter(_clientSocket);
     }
+    #endregion
+
+    #region methods
+    private void TbxCommand_OnTextChanged(object? sender, TextChangedEventArgs e)
+    {
+        if (!string.IsNullOrEmpty(TbxCommand.Text))
+        {
+            TblPlaceHolderCommand.IsVisible = false;
+        }
+        else
+        {
+            TblPlaceHolderCommand.IsVisible = true;
+        }
+    }
+
+    private void BtnClearCommand_OnClick(object? sender, RoutedEventArgs e)
+    {
+        TbxCommand.Text = "";
+        TbxCommand.Focus();
+        TblPlaceHolderCommand.IsVisible = true;
+    }
+    
+    private void BtnConnect_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (LblStatus.Content == "Connected")
+        {
+            //MessageBox.Show("Socket is already connected!!!", "SOCKET IS CONNECTED", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        else
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Loopback, 4711);
+
+            _clientSocket.Connect(endPoint);
+            TbxResponse.Text = "Connected to server";
+            
+            LblStatus.Content = "Connected";
+            LblStatus.Foreground = new SolidColorBrush(Color.FromRgb(20, 157, 77));
+        }
+    }
+
+    private void BtnSendCommand_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (LblStatus.Content == "Connected")
+        {
+            if (!string.IsNullOrEmpty(TbxCommand.Text))
+            {
+                if (TbxCommand.Text == "exit")
+                {
+                    LblStatus.Content = "Not Connected";
+                    LblStatus.Foreground = new SolidColorBrush(Color.FromRgb(255, 72, 111));
+                }
+                
+                _writer.SendText(TbxCommand.Text);
+
+                string response = _reader.ReceiveText();
+
+                TbxResponse.Text = response;
+                
+                TbxCommand.Text = "";
+            }
+            else
+            {
+                //MessageBox.Show("Please enter a command!!!", "NO COMMAND", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        else
+        {
+            //MessageBox.Show("Client is not connected to server!!!", "CLIENT IS NOT CONNECTED", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+    #endregion
 }
---
+++ JACService.Core/FileServiceLogger.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace JAC.Service.Core
+{
+    public class FileServiceLogger : IServiceLogger
+    {
+        #region instancevariable
+        readonly ConsoleColor _originalColor = Console.ForegroundColor;
+        #endregion
+        
+        #region constant
+        const string FILEPATH = @"..\ServiceInformation.txt";
+        #endregion
+
+        #region methods
+        public void LogServiceInfo(string message)
+        {
+            LogMessageWithColor(message, ConsoleColor.Red);
+        }
+
+        public void LogRequestInfo(string message)
+        {
+            LogMessageWithColor(message, ConsoleColor.Green);
+        }
+
+        private void LogMessageWithColor(string message, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ForegroundColor = _originalColor;
+
+            try
+            {
+                File.AppendAllText(FILEPATH, message + Environment.NewLine);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error occured: {ex.Message}");
+            }
+        }
+        #endregion
+    }
+}
